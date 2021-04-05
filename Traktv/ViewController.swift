30cdf9cea4749a98921a722ae1214d181de64b54
Ismail <codeismail@gmail.com>
2021-04-05 12:01:46 +0100

    Initial Commit

---
+++ Traktv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traktv
+//
+//  Created by Ismail on 05/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
